37f8847478eaa7d054e74a62d37a444e8e47986b
Andrew Conklin <arconkli@gmail.com>
2024-11-18 20:04:42 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import KeeKohWebsite from "./KeeKohWebsite.jsx";
-import ItAllFalls from "./ItAllFalls.jsx";
+import KeeKohWebsite from "./KeekohWebsite.jsx"; 
+import MusicLinksPage from "./MusicLinksPage.jsx"; 
+import ItAllFalls from "./ItAllFalls.jsx"; 
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<KeeKohWebsite />} />
-        <Route path="/music" element={<ItAllFalls />} />
+        <Route path="/music" element={<MusicLinksPage />} />
+        <Route path="/itallfalls" element={<ItAllFalls />} /> 
       </Routes>
     </BrowserRouter>
   );
